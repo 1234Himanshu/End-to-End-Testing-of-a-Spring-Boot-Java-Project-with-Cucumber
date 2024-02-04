bb00b2119b9cc5c2d29976f4daed55b58843e3c7
1234Himanshu <158808664+1234Himanshu@users.noreply.github.com>
2024-02-04 22:06:48 +0530

    Update DemohystrixApplication.java

--- Demoe2testing/demoe2etesting/src/main/java/org/wj/prajumsook/DemohystrixApplication.java
+++ Demoe2testing/demoe2etesting/src/main/java/org/wj/prajumsook/DemohystrixApplication.java
@@ -1,33 +1,17 @@
-package org.wj.prajumsook.config;
+package org.wj.prajumsook;
 
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-import org.wj.prajumsook.entity.Post;
-import org.wj.prajumsook.service.PostService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-import java.util.UUID;
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+public class DemohystrixApplication {
 
-@Component
-@Profile("test")
-@Log4j2
-public class InitTestData implements ApplicationListener<ApplicationReadyEvent> {
+	public static void main(String[] args) {
+		SpringApplication.run(DemohystrixApplication.class, args);
+	}
 
-    @Autowired
-    private PostService service;
-
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        service.deleteAll();
-        service.save(new Post(UUID.randomUUID().toString(), "Post Nr: 1 title", "Post Nr: 1 content"));
-        service.save(new Post(UUID.randomUUID().toString(), "Post Nr: 2 title", "Post Nr: 2 content"));
-        service.save(new Post(UUID.randomUUID().toString(), "Post Nr: 3 title", "Post Nr: 3 content"));
-        service.save(new Post(UUID.randomUUID().toString(), "Post Nr: 4 title", "Post Nr: 4 content"));
-        service.save(new Post(UUID.randomUUID().toString(), "Post Nr: 5 title", "Post Nr: 5 content"));
-
-        log.info("5 Post saved");
-    }
 }
